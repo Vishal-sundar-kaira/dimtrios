35cd1397e798f3a7a00b4f00a64247d1c1908968
vishal kaira <103746076+Vishal-sundar-kaira@users.noreply.github.com>
2022-10-19 19:36:12 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Pune ke trip ke liye project.
+# DIMITRIOS
+Today's world is pacing rapidly towards AI and ML. The internet has become a necessity in every domain hence there is a need for AI which can automate the tasks. We need to make this technology easily accessible to the users by linking it with web pages.We need to fulfil the demand of combination of AI and web technology.
+Here's DIMITRIOS which comprises of four important AI integrated tools namely, People Counter, Color Detection, Text Summarization and Language Detection. Check out our project here!
--- detect.py
+++ detect.py
@@ -1,10 +1,14 @@
 
+import re
 from flask import Flask, render_template, url_for, request
 import cv2
 import time
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 def face(url):
+    c=0
     start=time.time()
     face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
     nurl="./static/styles/"+url
@@ -15,6 +19,7 @@ def face(url):
     face=face_cascade.detectMultiScale(grayimg,1.1,5)
     for x,y,w,h in face:
         img=cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)
+        c=c+1
     # cv2.imshow("Gray",img)
     # end=time.time()
     # timing=end-start
@@ -22,6 +27,7 @@ def face(url):
     # cv2.waitKey(0)
     # cv2.destroyAllWindows()
     cv2.imwrite('./static/styles/newfile.png',img)
+    return c
 
 def summarize(text):
     import nltk
@@ -148,6 +154,69 @@ def language(txt):
 
     output = lang[0]
     return output
+
+
+
+#declaring global variables (are used later on)
+clicked = False
+r = g = b = xpos = ypos = 0
+
+#Reading csv file with pandas and giving names to each column
+index=["color","color_name","hex","R","G","B"]
+csv = pd.read_csv('color_dataset.csv', names=index, header=None)
+
+#function to calculate minimum distance from all colors and get the most matching color
+def getColorName(R,G,B):
+    minimum = 10000
+    for i in range(len(csv)):
+        d = abs(R- int(csv.loc[i,"R"])) + abs(G- int(csv.loc[i,"G"]))+ abs(B- int(csv.loc[i,"B"]))
+        if(d<=minimum):
+            minimum = d
+            cname = csv.loc[i,"color_name"]
+    return cname
+
+#function to get x,y coordinates of mouse double click
+def draw_function(event, x,y,flags,param):
+    if event == cv2.EVENT_LBUTTONDBLCLK:
+        global b,g,r,xpos,ypos,clicked
+        clicked = True
+        xpos = x
+        ypos = y
+        b,g,r = img[y,x]
+        b = int(b)
+        g = int(g)
+        r = int(r)
+
+def processor(img):
+
+    global clicked
+    clicked = False
+    cv2.namedWindow('image')
+    cv2.setMouseCallback('image', draw_function)
+
+    while(True):
+        cv2.imshow("image", img)
+        cv2.imwrite('./static/styles/oldfile1.png',img)
+        if clicked:
+            cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
+            text = getColorName(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
+            
+            #cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
+            cv2.putText(img, text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
+           
+            #For very light colours we will display text in black colour
+            if(r+g+b>=600):
+                cv2.putText(img, text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
+           
+            clicked=False
+        #Break the loop when user hits 'esc' key 
+        if cv2.waitKey(20) & 0xFF ==27:
+            break
+    cv2.destroyAllWindows()
+
+
+#Reading the image with opencv
+
 @app.route("/",methods=["GET","POST"])
 def index():
     return render_template("homepage.html")
@@ -161,8 +230,8 @@ def faced():
     print("hello world")
     if request.method=="POST":
         url=request.form.get("url")
-        face(url)
-        return render_template("index.html",reimg1="oldfile.png",reimg2="newfile.png")
+        obj=face(url)
+        return render_template("index.html",reimg1="oldfile.png",reimg2="newfile.png",obj=obj)
     return render_template("index.html")
 
 @app.route("/text",methods=["GET","POST"])
@@ -183,6 +252,12 @@ def lang():
     return render_template("index4.html")
 @app.route("/color",methods=["GET","POST"])
 def color():
+    if request.method=="POST":
+        global img
+        url=request.form.get("url")
+        img = cv2.imread("./static/styles/"+url)
+        processor(img)
+        return render_template("index3.html",cdold="oldfile1.png")
     return render_template("index3.html")
 
 
@@ -193,4 +268,4 @@ def color():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+   app.run(debug=True)
